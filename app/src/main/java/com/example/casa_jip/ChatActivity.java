03357ea20590d1ca6bce3b5ee4a2a81189675e08
Ju Mong Ko <jk2299@cornell.edu>
2020-06-28 01:36:04 +0900

    Chat Dev basic functionality

---
+++ app/src/main/java/com/example/casa_jip/ChatActivity.java
@@ -0,0 +1,100 @@
+package com.example.casa_jip;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<ChatData> chatList;
+    private String nickname = "nickname_1";
+    private EditText EditText_chat;
+    private Button Button_send;
+    private DatabaseReference myRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        EditText_chat = findViewById(R.id.EditText_chat);
+        Button_send = findViewById(R.id.Button_send);
+
+        Button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = EditText_chat.getText().toString();
+
+                if(message != null){
+                    ChatData chat = new ChatData();
+                    chat.setNickname(nickname);
+                    chat.setMessage(message);
+                    myRef.push().setValue(chat);
+                }
+            }
+        });
+
+        mRecyclerView = findViewById(R.id.chat_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        chatList = new ArrayList<>();
+        mAdapter = new ChatAdapter(chatList, ChatActivity.this, nickname);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("CHATCHAT", dataSnapshot.getValue().toString());
+                ChatData chat = dataSnapshot.getValue(ChatData.class);
+                ((ChatAdapter) mAdapter).addChat(chat);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
