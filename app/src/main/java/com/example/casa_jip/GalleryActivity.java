29752cd84436327f7e36992f2765c658bc584aa7
Ju Mong Ko <jk2299@cornell.edu>
2020-07-06 01:40:05 +0900

    galleryDev Rebase

--- app/src/main/java/com/example/casa_jip/GalleryActivity.java
+++ app/src/main/java/com/example/casa_jip/GalleryActivity.java
@@ -7,10 +7,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,32 +33,67 @@ public class GalleryActivity extends AppCompatActivity {
     private GalleryAdapter mAdapter;
     private LinearLayoutManager mLayoutManager;
     private List<ImageData> ImageList;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
 
-        ImageList = new ArrayList<>();
-        ImageList.add(new ImageData(R.drawable.alps));
-        ImageList.add(new ImageData(R.drawable.avocados));
-        ImageList.add(new ImageData(R.drawable.bentley));
-        ImageList.add(new ImageData(R.drawable.bibimbop));
-        ImageList.add(new ImageData(R.drawable.camerajpg));
-        ImageList.add(new ImageData(R.drawable.iphone11));
-        ImageList.add(new ImageData(R.drawable.kimchi));
-        ImageList.add(new ImageData(R.drawable.orange));
-        ImageList.add(new ImageData(R.drawable.programmer));
-        ImageList.add(new ImageData(R.drawable.travel));
-        ImageList.add(new ImageData(R.drawable.vietnam));
+        //Upload Button OnClickListener
+        FloatingActionButton fab = findViewById(R.id.button_upload);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Button for uploading images", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
+        ImageList = new ArrayList<>();
 
         mRecyclerView = findViewById(R.id.gallery_recycler_view);
-        mAdapter = new GalleryAdapter(this,ImageList);
+        mAdapter = new GalleryAdapter(ImageList, GalleryActivity.this);
         mLayoutManager = new GridLayoutManager(this,3);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+
+        //Firebase Cloud Storage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReferenceFromUrl("gs://casajip-c4cc9.appspot.com");
+
+        storageRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference prefix : listResult.getPrefixes()) {
+                            // All the prefixes under listRef.
+                            // You may call listAll() recursively on them.
+                            Log.d("Prefix", prefix.toString());
+                        }
+
+                        for (StorageReference item : listResult.getItems()) {
+                            // All the items under listRef.
+                            Log.d("Item", item.toString());
+                            Task<Uri> itemUrl = item.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String stringUrl = uri.toString();
+                                    ImageData itemImage = new ImageData(stringUrl);
+                                    mAdapter.addImage(itemImage);
+                                }
+                            });
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Uh-oh, an error occurred!
+                    }
+                });
+
 
         //Initialize
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
